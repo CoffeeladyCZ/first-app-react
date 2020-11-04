2fb03e978427986d8e90786e826cbd6ae5be992d
Marcela <62770219+CoffeeladyCZ@users.noreply.github.com>
2020-11-04 22:26:01 +0100

    první commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
+import logo from './coffee_logo.png';
 import './App.css';
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          #butfirstcoffee
         </p>
         <a
           className="App-link"
@@ -15,11 +16,15 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Link, který budu asi potřebovat
         </a>
       </header>
     </div>
   );
 }
+ReactDOM.render(
+  <h2>Netuším jak smazat tehle margin</h2>, 
+  document.getElementById('test')
+);
 
 export default App;
