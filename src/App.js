e581a80cf16dd8ee40f8193ab3e06a75ffa5fbce
Marcela <62770219+CoffeeladyCZ@users.noreply.github.com>
2020-11-12 02:09:00 +0100

    Adding new buttons and boxes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import React from 'react';
 import logo from './coffee_logo.png';
 import './App.css';
 import Button from './Button';
+import ProductBox from './Product-box';
 
 // this is our data which we are passing as props to <Button component />
 const button = {
   title: "Detail",
-  type: "buy"
+  type: "green"
+}
+const greyButton = {
+  title: "Detail",
+  type: "grey"
+}
+const productBox = {
+  title: "Box"
 }
 
 function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
+      <ProductBox div={productBox} />
+      <ProductBox div={productBox} />
+      <ProductBox div={productBox} />
+      <Button button={greyButton}/>
       <Button button={button}/>
+
     </div>
   );
 }
---
+++ src/Product-box.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Product-box.css';
+
+class ProductBox extends React.Component {
+    render() {
+        return <div className="product-box"></div>
+        
+    }
+}
+
+export default ProductBox; 
